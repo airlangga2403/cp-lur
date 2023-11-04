a0cabd90da8d0c73ddfac8c01d4c56dcd0cd562b
Zeeus <airlanggapermana96@gmail.com>
2023-11-05 02:00:06 +0700

    Solve Plus Minus Challange

---
+++ src/com/javacp/masters/hackerrank/dsa/PlusMinus.java
@@ -0,0 +1,65 @@
+package com.javacp.masters.hackerrank.dsa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class PlusMinus {
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        int n = arr.size();
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+
+        for (int num : arr) {
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            } else {
+                zeroCount++;
+            }
+        }
+
+        double positiveRatio = (double) positiveCount / n;
+        double negativeRatio = (double) negativeCount / n;
+        double zeroRatio = (double) zeroCount / n;
+
+        System.out.printf("%.6f\n", positiveRatio);
+        System.out.printf("%.6f\n", negativeRatio);
+        System.out.printf("%.6f\n", zeroRatio);
+        
+    }
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        PlusMinus.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
